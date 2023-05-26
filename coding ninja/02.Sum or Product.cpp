b5bd747c6b27c353c8b538864c568600ca04918c
AnupRaj0210 <133004821+AnupRaj0210@users.noreply.github.com>
2023-05-27 00:27:48 +0530

    Create 02.Sum or Product.cpp

---
+++ coding ninja/02.Sum or Product.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h> 
+long long int sumOrProduct(long long int n, long long int q)
+{
+	int sum;
+	int product;
+	int module;
+	// Write your code here.
+
+	if(q==1){
+		long long int sum=0;
+		for(int i=0;i<=n;i++){
+			sum+=i;
+		}
+		return sum;
+	}
+	else if(q==2){
+		long long int product=1;
+		const unsigned int p=1000000007;
+		for(int i=1;i<=n;i++){
+			product=(product*i)%p;
+		}
+		return product;
+        }
+        
+}
