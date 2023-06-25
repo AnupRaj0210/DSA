9d4c4d9b750e6a58c9b759d0d0ac1634c98b838f
AnupRaj <133004821+AnupRaj0210@users.noreply.github.com>
2023-06-25 19:42:53 +0530

    Create 03.Solve Me First.cpp

---
+++ Assignment 1/03.Solve Me First.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a,b;
+    cin>>a>>b;
+    int sum=0;
+    sum=a+b;
+    cout<<sum;  
+    return 0;
+}
