e76e09091961effa18229da6b4dc268dc965abb0
AnupRaj <133004821+AnupRaj0210@users.noreply.github.com>
2023-06-25 19:47:27 +0530

    Create 03.swap two numbers with a third variable.cpp

---
+++ Assignment 1/03.swap two numbers with a third variable.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a=2,b=3;
+    cout<<"before swapping"<<endl;
+    cout<<"a="<<a<<",b="<<b<<endl;
+    int temp;
+    temp=a;
+    a=b;
+    b=temp;
+    cout<<"after swapping"<<endl;
+    cout<<"a="<<a<<",b="<<b<<endl;
+
+    return 0;
+}
