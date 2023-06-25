3dfe880478f6d07e464bc7e6b542a6e02f32b9cb
AnupRaj <133004821+AnupRaj0210@users.noreply.github.com>
2023-06-25 23:34:39 +0530

    Update 03.Write a program to check whether a given number is prime or not.cpp

--- Assignment 2 /03.Write a program to check whether a given number is prime or not.cpp
+++ Assignment 2 /03.Write a program to check whether a given number is prime or not.cpp
@@ -1,1 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+    int a,i,n;
+    cin>>n;
+    int count=0;
+    for(i=1;i<=n;i++){
+        if(n%i==0){
+            count++;
+        }
+    }
+    if(count==2){
+        cout<<"primenumber";
+    }
+    else{
+        cout<<"notprimenumber";
+    }
+    return 0;
+}
